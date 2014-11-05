348c962ec7c35cda90307ce09db6e01ce3e9150c
Vitali <Vitali_Usik@epam.com>
2014-11-05 14:40:44 +0300

    [feature019] add mask and validation for all_forms

--- js/script.js
+++ js/script.js
@@ -60,4 +60,169 @@ $(function() {
 
 });
 
+//  Phone Mask 2
+$(function() {
+  
+  if($('#user_phone2').exists()){
+    
+    $('#user_phone2').each(function(){
+      $(this).mask("(99) 999-99-99");
+    });
+    
+  }
+  
+  if($('.phone_form2').exists()){
+    
+    var form = $('.phone_form2'),
+      btn = form.find('.btn_submit2');
+    
+    form.find('.rfield2').addClass('empty_field2');
+  
+    setInterval(function(){
+    
+      if($('#user_phone2').exists()){
+        var pmc = $('#user_phone2');
+        if ( (pmc.val().indexOf("_") != -1) || pmc.val() == '' ) {
+          pmc.addClass('empty_field2');
+        } else {
+            pmc.removeClass('empty_field2');
+        }
+      }
+      
+      var sizeEmpty = form.find('.empty_field2').size();
+      
+      if(sizeEmpty > 0){
+        if(btn.hasClass('disabled2')){
+          return false
+        } else {
+          btn.addClass('disabled2')
+        }
+      } else {
+        btn.removeClass('disabled2')
+      }
+      
+    },200);
+
+    btn.click(function(){
+      if($(this).hasClass('disabled2')){
+        return false
+      } else {
+        form.submit();
+      }
+    });
+    
+  }
+
+});
+
+//  Phone Mask 3
+$(function() {
+  
+  if($('#user_phone3').exists()){
+    
+    $('#user_phone3').each(function(){
+      $(this).mask("(99) 999-99-99");
+    });
+    
+  }
+  
+  if($('.phone_form3').exists()){
+    
+    var form = $('.phone_form3'),
+      btn = form.find('.btn_submit3');
+    
+    form.find('.rfield3').addClass('empty_field3');
+  
+    setInterval(function(){
+    
+      if($('#user_phone2').exists()){
+        var pmc = $('#user_phone3');
+        if ( (pmc.val().indexOf("_") != -1) || pmc.val() == '' ) {
+          pmc.addClass('empty_field3');
+        } else {
+            pmc.removeClass('empty_field3');
+        }
+      }
+      
+      var sizeEmpty = form.find('.empty_field3').size();
+      
+      if(sizeEmpty > 0){
+        if(btn.hasClass('disabled3')){
+          return false
+        } else {
+          btn.addClass('disabled3')
+        }
+      } else {
+        btn.removeClass('disabled3')
+      }
+      
+    },200);
+
+    btn.click(function(){
+      if($(this).hasClass('disabled3')){
+        return false
+      } else {
+        form.submit();
+      }
+    });
+    
+  }
+
+});
+
+//  Phone Mask4
+$(function() {
+  
+  if($('#user_phone4').exists()){
+    
+    $('#user_phone4').each(function(){
+      $(this).mask("(99) 999-99-99");
+    });
+    
+  }
+  
+  if($('.phone_form4').exists()){
+    
+    var form = $('.phone_form4'),
+      btn = form.find('.btn_submit4');
+    
+    form.find('.rfield4').addClass('empty_field4');
+  
+    setInterval(function(){
+    
+      if($('#user_phone4').exists()){
+        var pmc = $('#user_phone4');
+        if ( (pmc.val().indexOf("_") != -1) || pmc.val() == '' ) {
+          pmc.addClass('empty_field4');
+        } else {
+            pmc.removeClass('empty_field4');
+        }
+      }
+      
+      var sizeEmpty = form.find('.empty_field4').size();
+      
+      if(sizeEmpty > 0){
+        if(btn.hasClass('disabled4')){
+          return false
+        } else {
+          btn.addClass('disabled4')
+        }
+      } else {
+        btn.removeClass('disabled4')
+      }
+      
+    },200);
+
+    btn.click(function(){
+      if($(this).hasClass('disabled4')){
+        return false
+      } else {
+        form.submit();
+      }
+    });
+    
+  }
+
+});
+
 })( jQuery );
